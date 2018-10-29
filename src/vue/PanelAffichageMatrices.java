76a504e2c642f10338041220eb0e671df2c73876
Naaviiss <armengaudmarion@gmail.com>
2018-10-29 10:36:30 +0100

    mise en place de quelques éléments

---
+++ src/vue/PanelAffichageMatrices.java
@@ -0,0 +1,12 @@
+package vue;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PanelAffichageMatrices extends JPanel{
+	//private Matrice chMatrice;
+	private JButton []boutonsSelection; //les boutons pour choisir la ligne à modifier
+	public PanelAffichageMatrices(/*Matrice pMatrice*/) {
+		
+	}
+}
---
+++ src/vue/PanelGauss.java
@@ -0,0 +1,11 @@
+package vue;
+
+import javax.swing.JPanel;
+
+public class PanelGauss extends JPanel{
+	private PanelAffichageMatrices affichageMatrices; //là où les matrices seront affichées
+	
+	public PanelGauss(/*Matrice pMatrice*/) {
+		
+	}
+}
