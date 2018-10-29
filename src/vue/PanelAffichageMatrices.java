0548364a4a447c48e9c37ef758317eb8d342908f
Naaviiss <armengaudmarion@gmail.com>
2018-10-29 22:10:04 +0100

    mise en place JScrollBar

--- src/vue/PanelAffichageMatrices.java
+++ src/vue/PanelAffichageMatrices.java
@@ -1,13 +1,21 @@
 package vue;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 
 public class PanelAffichageMatrices extends JPanel{
 	//private Matrice chMatriceBase;
 	//private Matrice chMatriceIdentite;
 	private JButton []boutonsSelection; //les boutons pour choisir la ligne à modifier
+	
 	public PanelAffichageMatrices(/*Matrice pMatriceBase, Matrice pMatriceIdentite*/) {
 		
+		this.setBackground(new Color(0, 255, 0));
 	}
 }
--- src/vue/PanelGauss.java
+++ src/vue/PanelGauss.java
@@ -1,11 +1,31 @@
 package vue;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Insets;
+
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
 public class PanelGauss extends JPanel{
 	private PanelAffichageMatrices affichageMatrices; //là où les matrices seront affichées
+	private JScrollPane defil;//panneau defilant
 	
 	public PanelGauss(/*Matrice pMatrice*/) {
+		//gestionnaire et bordure
+		this.setLayout(new BorderLayout());
+		this.setBorder(new EmptyBorder(100, 100, 100, 100));
 		
+		affichageMatrices = new PanelAffichageMatrices(/*pMatrice*/);
+		defil = new JScrollPane(affichageMatrices);
+		
+		//ajout de la scrollbar
+		defil.setVerticalScrollBarPolicy(defil.VERTICAL_SCROLLBAR_ALWAYS);
+		this.add(defil,BorderLayout.WEST);
 	}
 }
