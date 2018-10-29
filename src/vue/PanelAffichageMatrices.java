0c029aa2ee08ef048e335e46578c159a4cad9d7a
Naaviiss <armengaudmarion@gmail.com>
2018-10-29 12:04:11 +0100

    avancée des classes

--- src/vue/PanelAffichageMatrices.java
+++ src/vue/PanelAffichageMatrices.java
@@ -4,9 +4,10 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class PanelAffichageMatrices extends JPanel{
-	//private Matrice chMatrice;
+	//private Matrice chMatriceBase;
+	//private Matrice chMatriceIdentite;
 	private JButton []boutonsSelection; //les boutons pour choisir la ligne à modifier
-	public PanelAffichageMatrices(/*Matrice pMatrice*/) {
+	public PanelAffichageMatrices(/*Matrice pMatriceBase, Matrice pMatriceIdentite*/) {
 		
 	}
 }
