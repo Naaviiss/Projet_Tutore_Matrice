efd41f07f3c35ee4c18c3f9f48ea212211d5e480
Naaviiss <armengaudmarion@gmail.com>
2018-11-18 17:23:51 +0100

    renommage noms

--- src/vue/PanelChoix.java
+++ src/vue/PanelChoix.java
@@ -7,22 +7,22 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
 public class PanelChoix extends JPanel implements ActionListener{
-	PanelTaille taille;
+	PanelTaille panTaille;
 	PanelGauss gauss;
-	PanelMatrice matrice;
+	PanelMatrice panMatrice;
 	
 	public PanelChoix() {
 		this.setLayout(new CardLayout());
 		
-		taille = new PanelTaille();
+		panTaille = new PanelTaille();
 		gauss = new PanelGauss();
-		matrice = new PanelMatrice();
-		this.add(taille, "panel taille");
+		panMatrice = new PanelMatrice();
+		this.add(panTaille, "panel taille");
 		this.add(gauss, "panel_gauss");
-		this.add(matrice, "panel_matrice");
+		this.add(panMatrice, "panel_matrice");
 		
-		taille.enregistreEcouteur(this);
-		matrice.enregistreEcouteur(this);
+		panTaille.enregistreEcouteur(this);
+		panMatrice.enregistreEcouteur(this);
 	}
 	
 	
@@ -33,14 +33,14 @@ public class PanelChoix extends JPanel implements ActionListener{
 	
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
-		   if (source == taille.valider)   {
-			   System.out.println(taille.getTaille());
-			   	if (taille.getTaille() == 3 || taille.getTaille() == 4 || taille.getTaille() == 5) {
+		   if (source == panTaille.valider)   {
+			   System.out.println(panTaille.getTaille());
+			   	if (panTaille.getTaille() == 3 || panTaille.getTaille() == 4 || panTaille.getTaille() == 5) {
 			   		((CardLayout) this.getLayout()).show(this,"panel_matrice");	
 		          }
 		          
 		      }
-		   if (source == matrice.valider)   {
+		   if (source == panMatrice.valider)   {
 		          ((CardLayout) this.getLayout()).show(this,"panel_gauss");
 		      }
 		  }//actionPerformed()
