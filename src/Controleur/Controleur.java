011421eb793ecf6ab2ad69797de404362abc9cc6
Wallnya <34969114+Wallnya@users.noreply.github.com>
2018-11-23 18:43:47 +0100

    synchronisation taille matrice bouton

--- src/Controleur/Controleur.java
+++ src/Controleur/Controleur.java
@@ -4,20 +4,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import modele.Data;
+import vue.PanelAffichage;
+import vue.PanelChoix;
 import vue.PanelMatrice;
 
 public class Controleur implements ActionListener{
 	
-	//faudra mettre tous les champs mais por l'instant je mets le mien
 	PanelMatrice chPanMatrice;
+	PanelChoix chPanelChoix;
 	
-	public Controleur(PanelMatrice pPanMatrice) {
+	public Controleur(PanelMatrice pPanMatrice, PanelChoix pPanChoix) {
 		chPanMatrice = pPanMatrice;
+		chPanelChoix = pPanChoix;
 	}
-	
+
 	public void actionPerformed(ActionEvent pEvt) {
+		if(pEvt.getActionCommand().equals(Data.CHOIX[1])) {
+			chPanelChoix.getCardLayout().show(chPanelChoix, "panel_taille");
+		}
+		if(pEvt.getActionCommand().equals(Data.VALIDER_PANEL_TAILLE)) {
+			chPanMatrice = new PanelMatrice(chPanelChoix.getPanTaille().getTaille());
+			chPanelChoix.add(chPanMatrice, "panel_matrice");
+			chPanelChoix.getCardLayout().show(chPanelChoix, "panel_matrice");
+		}
 		if(pEvt.getActionCommand().equals(Data.VALIDER_PANEL_MATRICE)) {
-			
+			chPanelChoix.getCardLayout().show(chPanelChoix, "panel_gauss");
 		}
 		if(pEvt.getActionCommand().equals(Data.VALIDER_PANEL_COMMANDES)) {
 			
