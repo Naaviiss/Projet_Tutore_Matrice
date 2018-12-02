dec458c8024ab4c10818b01783da96faa5669ad2
Naaviiss <armengaudmarion@gmail.com>
2018-12-02 18:38:25 +0100

    agrandissement panel taille

--- src/vue/PanelTaille.java
+++ src/vue/PanelTaille.java
@@ -1,6 +1,5 @@
 package vue;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 
@@ -8,39 +7,41 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import Controleur.Controleur;
 import modele.Data;
 
 public class PanelTaille extends JPanel{
-	private JButton valider = new JButton("valider");
-	private JComboBox comboPoids;
+    private JButton valider = new JButton("valider");
+    private JComboBox comboTaille;
 
-	private String taille[]= {"3","4","5"};
-	private JLabel monlabel;
-	
-	public PanelTaille(){		
-		monlabel = new JLabel("Choisir une taille de matrice entre 3 et 5");
-		this.setBorder(new EmptyBorder(300,300,300,300));
-		valider.setPreferredSize(new Dimension(100, 30));
-		comboPoids = new JComboBox(taille);
-		valider.setFont(new Font(Font.SERIF,Font.BOLD,10));
-		valider.setActionCommand(Data.VALIDER_PANEL_TAILLE);
-		
-		//ajout des champs au panel
-		this.add(monlabel);
-		this.add(comboPoids, "champs_taille");
-		this.add(valider, "valider");
-	}
-	
-	public int getTaille() {
-		int poids = comboPoids.getSelectedIndex();
-		return poids+3;
-	}
-	
-	public void enregistreEcouteur(Controleur parControleur){
-		valider.addActionListener(parControleur);
-	}
+    private String taille[]= {"3","4","5"};
+    private JLabel monlabel;
+
+    public PanelTaille(){
+        monlabel = new JLabel("Choisir une taille de matrice entre 3 et 5");
+        Font font = new Font(Font.SERIF, 20, 25);
+        monlabel.setFont(font);
+
+        this.setBorder(new EmptyBorder(300,300,300,300));
+        valider.setPreferredSize(new Dimension(120, 40));
+        comboTaille = new JComboBox(taille);
+        valider.setFont(new Font(Font.SERIF, 20, 25));
+        valider.setActionCommand(Data.VALIDER_PANEL_TAILLE);
+
+        //ajout des champs au panel
+        this.add(monlabel);
+        this.add(comboTaille, "champs_taille");
+        this.add(valider, "valider");
+    }
+
+    public int getTaille() {
+        int poids = comboTaille.getSelectedIndex();
+        return poids+3;
+    }
+
+    public void enregistreEcouteur(Controleur parControleur){
+        valider.addActionListener(parControleur);
+    }
 }
