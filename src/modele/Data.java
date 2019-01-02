b87e4fb42a1bf04ec85fdf7af38810049bd364ac
nanacy <39811762+nanacy@users.noreply.github.com>
2019-01-02 20:16:12 +0100

    supp retour avant, add options d'aide

--- src/modele/Data.java
+++ src/modele/Data.java
@@ -5,7 +5,7 @@ public interface Data {
 	public final String [] TITRE_MENU_LISTE={"Simplex","Matrice"};
 	public final String [] CHOIX={"Simplex","Matrice"};
 	public final String [] TITRE_MATRICE={"Retour au menu principal","Outils", "Aide","Quitter"};
-	public final String [] TITRE_MATRICE_LISTE={"Revenir en avant", "Revenir en arriere"};
+	public final String [] TITRE_MATRICE_LISTE={"Revenir en arriere", "+ (agrandir)", "- (rétrécir)","Recommencer le calcul"};
 
 	public final String[] LIGNES = {"L1","L2","L3","L4","L5"};
 	public final String[] INTITULES = {"Matrice","Identité","Calcul","Commentaire"};
